3002a44f8440864cfd4e8a43af6cddd38ddf463b
xiaoguan <100571246+xiaoguan0@users.noreply.github.com>
2022-08-14 12:02:29 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import footere from './components/footer.vue'
+import main from './components/main.vue'
+import comments from './components/comments.vue'
+import all_articles from './components/all_articles.vue'
+import headere from './components/header.vue'
+
+</script>
+
+<template>
+	<headere />
+	<footere name="Xiaogaun" timefirst="2021" timeafter="2022"/>
+</template> 
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
